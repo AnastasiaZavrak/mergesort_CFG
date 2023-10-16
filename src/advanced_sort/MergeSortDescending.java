f4f1d5438e0e0abf956a7ca1583f75a29e56f06b
AnastasiaZavrak <haziest.claques_0f@icloud.com>
2023-10-16 17:33:42 +0100

    added descending sort, and a menu-driven application

--- src/advanced_sort/MergeSortDescending.java
+++ src/advanced_sort/MergeSortDescending.java
@@ -4,23 +4,20 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-// I couldn't add the improvement here, as it wouldn't work, however, mergeSort will still do pretty good on sorting
-public class MergeSortIncreasing {
+public class MergeSortDescending {
     public static <T extends Comparable<T>> void mergeSort(final List<T> list, final Comparator<T> comparator) {
         int listLength = list.size();
         if (listLength <= 1) {
             return;
         }
         int middleOfList = listLength / 2;
-        // if e.g. length = 9, then middle of the array would be 4, and (array length - middle) = 5
-        List<T> leftPartArray = new ArrayList<>(list.subList(0, listLength - middleOfList));
-        List<T> rightPartArray = new ArrayList<>(list.subList(listLength - middleOfList, listLength));
 
-        //recursive calls to merge left and right parts of the array
+        List<T> leftPartArray = new ArrayList<>(list.subList(0, middleOfList));
+        List<T> rightPartArray = new ArrayList<>(list.subList(middleOfList, listLength));
+
         mergeSort(leftPartArray, comparator);
         mergeSort(rightPartArray, comparator);
 
-        //merge left and right parts of the array
         merge(list, leftPartArray, rightPartArray, comparator);
     }
 
@@ -28,14 +25,13 @@ public class MergeSortIncreasing {
         int leftListLength = leftPartArray.size();
         int rightListLength = rightPartArray.size();
 
-        // we need 3 pointers: 1 for left part, 1 for right part, and 1 for the merged array
         int leftPointer = 0;
         int rightPointer = 0;
         int mergedPointer = 0;
 
         while (leftPointer < leftListLength && rightPointer < rightListLength) {
-            //comparing elements of the both parts so that the smallest one goes to the merged array
-            if (leftPartArray.get(leftPointer).compareTo(rightPartArray.get(rightPointer)) <= 0) {
+            // Compare elements in descending order
+            if (comparator.compare(leftPartArray.get(leftPointer), rightPartArray.get(rightPointer)) >= 0) {
                 list.set(mergedPointer, leftPartArray.get(leftPointer));
                 leftPointer++;
             } else {
@@ -45,7 +41,6 @@ public class MergeSortIncreasing {
             mergedPointer++;
         }
 
-        //add any leftover elements from either or the arrays
         while (leftPointer < leftListLength) {
             list.set(mergedPointer, leftPartArray.get(leftPointer));
             leftPointer++;
--- src/advanced_sort/TestClass.java
+++ src/advanced_sort/TestClass.java
@@ -3,24 +3,74 @@ package advanced_sort;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class TestClass {
     public static void main(String[] args) {
         Comparator<Student> gradeComparator = Comparator.comparingInt(Student::getGrade);
+        List<Student> students = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
 
-        List<Student> studentList = new ArrayList<>();;
-        studentList.add(new Student("Alice Grey", 97));
-        studentList.add(new Student("Tim Brady", 50));
-        studentList.add(new Student("Meredith Loon", 72));
-        studentList.add(new Student("Emma Johnson", 81));
-        studentList.add(new Student("Benjamin Patel", 43));
-        studentList.add(new Student("Olivia Williams", 90));
-        studentList.add(new Student("Liam Smith", 82));
+        // Add initial students to the list
+        students.add(new Student("Alice Grey", 97));
+        students.add(new Student("Tim Brady", 50));
+        students.add(new Student("Meredith Loon", 72));
+        students.add(new Student("Emma Johnson", 81));
+        students.add(new Student("Benjamin Patel", 43));
+        students.add(new Student("Olivia Williams", 90));
+        students.add(new Student("Liam Smith", 82));
 
-        MergeSortIncreasing.mergeSort(studentList, gradeComparator);
-        System.out.println("Students' grades in increasing order:");
-        for (Student student : studentList) {
-            System.out.println(student.getName() + ": " + student.getGrade());
+        while (isRunning) {
+            System.out.println("""
+                    Menu:
+                    1. Add student
+                    2. Print students by grade (ascending)
+                    3. Print students by grade (descending)
+                    4. Exit""");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // to consume newline character
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter student name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter student grade: ");
+                    int grade = scanner.nextInt();
+                    students.add(new Student(name, grade));
+                    System.out.println("Student added successfully.");
+                    break;
+
+                case 2:
+                    MergeSortAscending.mergeSort(students, gradeComparator);
+                    printStudents(students, "ascending order");
+                    break;
+
+                case 3:
+                    MergeSortDescending.mergeSort(students, gradeComparator);
+                    printStudents(students, "descending order");
+                    break;
+
+                case 4:
+                    System.out.println("Exiting the program.");
+                    scanner.close();
+                    isRunning = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+                    break;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printStudents(List<Student> students, String order) {
+        System.out.println("Students sorted in " + order + " by grade:");
+        for (Student student : students) {
+            System.out.println("Name: " + student.getName() + ", Grade: " + student.getGrade());
         }
     }
 }
